eace60af5a092eefe294ec43837d2fba9e12064e
aceld <danbing_at@163.com>
2018-03-05 09:01:12 +0800

    添加url静态路径过滤

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
-	_ "loveHome/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	_ "loveHome/routers"
+	"net/http"
+	"strings"
 )
 
 func main() {
+	ignoreStaticPath()
+
 	beego.Run()
 }
 
+func ignoreStaticPath() {
+
+	//透明static
+
+	beego.InsertFilter("/", beego.BeforeRouter, TransparentStatic)
+	beego.InsertFilter("/*", beego.BeforeRouter, TransparentStatic)
+
+}
+
+func TransparentStatic(ctx *context.Context) {
+	orpath := ctx.Request.URL.Path
+
+	beego.Debug("request url: ", orpath)
+
+	//如果请求uri还有api字段,说明是指令应该取消静态资源路径重定向
+	if strings.Index(orpath, "api") >= 0 {
+		return
+	}
+
+	//  /index.html ---> /static/html/index.html
+	http.ServeFile(ctx.ResponseWriter, ctx.Request, "static/html/"+ctx.Request.URL.Path)
+}
